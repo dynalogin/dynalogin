73e12edc13c1e0785f464ab951de90a22f5a5c3b
Daniel Pocock <daniel@pocock.com.au>
2013-02-05 19:05:18 +0100

    Clean up PHP code, begin adding TLS support

--- php/dynalogin.php
+++ php/dynalogin.php
@@ -22,15 +22,19 @@ function dynalogin_read($sock) {
 }
 
 function dynalogin_try_command($server, $port, $scheme, $use_tls, $command) {
-  $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-  socket_connect($sock, $server, $port);
+    if($use_tls) {
+        $sock = fsockopen("tls://".$server, $port, $errno, $errstr);
+    } else {  
+        $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_connect($sock, $server, $port);
+    }
 
   // read greeting
   $greeting_code = dynalogin_read($sock);
 
   if($greeting_code == 220) {
     // send auth request
-    socket_write($sock, $command);
+    socket_write($sock, $command."\n");
 
     // check response
     $response_code = dynalogin_read($sock);
@@ -43,20 +47,20 @@ function dynalogin_try_command($server, $port, $scheme, $use_tls, $command) {
     echo "bad greeting";
   }
 
-  // quit
-//  socket_close($sock);
+  socket_write($sock, "QUIT\n");
+  socket_close($sock);
   return $logged_in;
 
 }
 
 function dynalogin_auth($user, $code, $server, $port, $scheme = "HOTP", $use_tls = false) {
-    $request = "UDATA $scheme $user $code\n";
+    $request = "UDATA $scheme $user $code";
     return dynalogin_try_command($server, $port, $scheme, $use_tls, $request);
 }
 
 function dynalogin_auth_digest($user, $realm, $response, $digest_suffix,
     $server, $port, $scheme = "HOTP", $use_tls = false) {
-    $request = "UDATA ".$scheme."-DIGEST $user $realm $response $digest_suffix\n";
+    $request = "UDATA ".$scheme."-DIGEST $user $realm $response $digest_suffix";
     return dynalogin_try_command($server, $port, $scheme, $use_tls, $request);
 }
 
