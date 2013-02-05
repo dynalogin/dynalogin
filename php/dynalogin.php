4d98b8ef50d27c59c12771a22c8d6b98981adff0
Daniel Pocock <daniel@pocock.com.au>
2013-02-05 20:06:48 +0100

    Finish TLS support in PHP client code

--- php/dynalogin.php
+++ php/dynalogin.php
@@ -2,66 +2,73 @@
 
 
 
-function dynalogin_read($sock) {
-  if(!socket_last_error($sock)) {
-    while($line = socket_read($sock, 512, PHP_NORMAL_READ)) {
-#      echo "<p>".$line."</p>";
-      if(!preg_match("/^(\d\d\d)([- ])(.*)\$/", $line, $elements)) {
-        echo "bad data line\n";
-        return FALSE;
-      }
-      $code = $elements[1];
-      $last_line = ($elements[2] == " ");
-      $msg = $elements[3];
-      if($last_line) {
-        return $code;
-      }
+function dynalogin_read($fp) {
+    if(!feof($fp)) {
+        while($line = fgets($fp, 512)) {
+#        echo "<p>".$line."</p>";
+            if(!preg_match("/^(\d\d\d)([- ])(.*)\$/", $line, $elements)) {
+                echo "bad data line\n";
+                return FALSE;
+            }
+            $code = $elements[1];
+            $last_line = ($elements[2] == " ");
+            $msg = $elements[3];
+            if($last_line) {
+                return $code;
+            }
+        }
     }
-  }
-  return FALSE;
+    return FALSE;
 }
 
-function dynalogin_try_command($server, $port, $scheme, $use_tls, $command) {
+function dynalogin_write($fp, $msg) {
+    fwrite($fp, $msg);
+}
+
+function dynalogin_try_command($server, $port, $use_tls, $command) {
     if($use_tls) {
-        $sock = fsockopen("tls://".$server, $port, $errno, $errstr);
+        $fp = stream_socket_client("tls://".$server.':'.$port, $errno, $errstr);
     } else {  
-        $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        socket_connect($sock, $server, $port);
+        $fp = stream_socket_client("tcp://".$server.':'.$port, $errno, $errstr);
     }
+    if(!$fp)
+        return 0;
 
-  // read greeting
-  $greeting_code = dynalogin_read($sock);
+    // read greeting
+    $greeting_code = dynalogin_read($fp);
 
-  if($greeting_code == 220) {
+    if($greeting_code == 220) {
     // send auth request
-    socket_write($sock, $command."\n");
+    dynalogin_write($fp, $command."\n");
 
     // check response
-    $response_code = dynalogin_read($sock);
+    $response_code = dynalogin_read($fp);
     if($response_code == 250)
-      $logged_in = 1;
+        $logged_in = 1;
     else
-      $logged_in = 0;
-  } else {
-    // bad greeting
-    echo "bad greeting";
-  }
+        $logged_in = 0;
+    } else {
+        // bad greeting
+        syslog(LOG_ERR, "dynalogin: bad greeting");
+    }
 
-  socket_write($sock, "QUIT\n");
-  socket_close($sock);
-  return $logged_in;
+    dynalogin_write($fp, "QUIT\n");
+    fclose($fp);
 
+    if($logged_in == 0)
+        syslog(LOG_WARNING, "dynalogin: login failure"); 
+    return $logged_in;
 }
 
 function dynalogin_auth($user, $code, $server, $port, $scheme = "HOTP", $use_tls = false) {
     $request = "UDATA $scheme $user $code";
-    return dynalogin_try_command($server, $port, $scheme, $use_tls, $request);
+    return dynalogin_try_command($server, $port, $use_tls, $request);
 }
 
 function dynalogin_auth_digest($user, $realm, $response, $digest_suffix,
     $server, $port, $scheme = "HOTP", $use_tls = false) {
     $request = "UDATA ".$scheme."-DIGEST $user $realm $response $digest_suffix";
-    return dynalogin_try_command($server, $port, $scheme, $use_tls, $request);
+    return dynalogin_try_command($server, $port, $use_tls, $request);
 }
 
 ?>
